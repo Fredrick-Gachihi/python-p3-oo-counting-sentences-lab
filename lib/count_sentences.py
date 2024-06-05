e39f978ffab5d038f1ed07860b0fa4abce17554a
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-06-05 17:04:26 +0300

    count lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-    def __init__(self, value=''):
-        if not isinstance(value, str):
-            raise ValueError("The value must be a string.")
+    def __init__(self, value = ""):
         self.value = value
 
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if type(new_value) != str:
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
     def is_sentence(self):
-        return self.value.endswith('.')
+        return True if self.value.endswith(".") else False
 
     def is_question(self):
-        return self.value.endswith('?')
+        return True if self.value.endswith("?") else False
 
     def is_exclamation(self):
-        return self.value.endswith('!')
+        return True if self.value.endswith("!") else False
 
     def count_sentences(self):
-        modified_value = self.value.replace(',', '.')
-        sentences = modified_value.split('.')
-        sentences = [s.strip() for s in sentences if s.strip()]
-        return len(sentences)
-    
+        str_list = self.value.split(" ")
+        count = 0
+        for i in str_list:
+            if i.endswith(".") or i.endswith("?") or i.endswith("!"):
+                count += 1
+        return count
